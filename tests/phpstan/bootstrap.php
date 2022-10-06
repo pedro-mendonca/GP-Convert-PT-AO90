06805dcf341d12eda637637d3641d41cfb4a087e
pedro-mendonca <ped.gaspar@gmail.com>
2022-10-06 15:27:51 +0100

    Add PHPStan bootstrap

---
+++ tests/phpstan/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * PHPStan bootstrap file
+ *
+ * @package GP_Convert_PT_AO90
+ */
+
+// Set plugin version.
+if ( ! defined( 'GP_CONVERT_PT_AO90_VERSION' ) ) {
+	define( 'GP_CONVERT_PT_AO90_VERSION', '1.2.0' );
+}
+
+// Set plugin required PHP version. Needed for PHP compatibility check for WordPress < 7.2.
+if ( ! defined( 'GP_CONVERT_PT_AO90_REQUIRED_PHP' ) ) {
+	define( 'GP_CONVERT_PT_AO90_REQUIRED_PHP', '7.2' );
+}
+
+// Require plugin main file.
+require_once 'gp-convert-pt-ao90.php';
