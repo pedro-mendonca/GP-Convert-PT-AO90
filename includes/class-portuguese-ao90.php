ff786249da4ccd2bd2eaabaebb6eea32fdd899f0
pedro-mendonca <ped.gaspar@gmail.com>
2023-11-28 10:34:43 +0000

    Stop using 'translation-row' template

--- includes/class-portuguese-ao90.php
+++ includes/class-portuguese-ao90.php
@@ -188,7 +188,6 @@ if ( ! class_exists( __NAMESPACE__ . '\Portuguese_AO90' ) ) {
 			unset( $args, $template_path );
 
 			$gp_templates = array(
-				'translation-row',
 				'translation-row-preview',
 			);
 
